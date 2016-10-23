98511bcce578a784d5834bbac8ec7f0eaf8462ed
Marocco2 <marocco2@live.it>
2016-10-23 11:47:14 +0200

    Update record.bat

--- record.bat
+++ record.bat
@@ -1,3 +1,3 @@
-start "" /B "livestreamer.exe" --hls-timeout 300.0 -f -o mkstream-%APPVEYOR_BUILD_NUMBER%.ts http://www.hitbox.tv/sscaitournament 720p & timeout /t 1600
+start "" /B "livestreamer.exe" --hls-timeout 300.0 -f -o mkstream-%APPVEYOR_BUILD_NUMBER%.ts http://www.hitbox.tv/MarcusKron 720p & timeout /t 1600
 IF ERRORLEVEL 0 Exit 1
 TASKKILL /IM livestreamer.exe
